8f8e82921ad4aa6de95a0143452746d5c4611737
Junwoong <chawnsnd@gmail.com>
2018-07-05 18:54:33 +0900

    프로젝트 생성

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main_header">
+        sadfas
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      msg: "afasdf"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
